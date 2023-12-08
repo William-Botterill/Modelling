a01f29a1d84432a86cdbaf5d79464b17971b0a89
William-Botterill <114944652+William-Botterill@users.noreply.github.com>
2023-12-08 19:45:51 +0000

    PI estimator using area

--- Monte Carlo Area/main.py
+++ Monte Carlo Area/main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
+# Execute using Fn+Shift+F10
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    # Total area will have height 3 and width 5 making total area 15
+    # Any coordinate that is within (x - 1.5)^2 + (y - 1.5)^2 < 1 will be within the circle of radius 1
+    # Any coordinate that is within 3 < x < 4 and 1 < y < 2 will be a part of the square
+
+    square_count = 0
+    circle_count = 0
+    area_count = 0
+
+    for i in range(5000000):
+        x = random.uniform(0, 3.01)
+        y = random.uniform(0, 2)
+
+        if (x - 1) ** 2 + (y - 1) ** 2 < 1:
+            circle_count += 1
+        elif 2.01 <= x <= 3.01 and 0 <= y <= 1:
+            square_count += 1
+        else:
+            area_count += 1
+
+    print("Square Count: ", square_count)
+    print("Area Count: ", area_count)
+    print("CircleCount: ", circle_count)
+    print("PI Estimate", circle_count / square_count)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
