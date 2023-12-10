5080d9065113e253fa66fca4b17b54b17aaecd58
William-Botterill <114944652+William-Botterill@users.noreply.github.com>
2023-12-10 14:49:04 +0000

    Seperating the Machine class definition and MainCode class

--- Machine Problem/Machine.java
+++ Machine Problem/Machine.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Machine {
     private int machineId;
     private int capacity;
@@ -6,16 +8,29 @@ public class Machine {
     private int processTime;
     private int processVariance;
     private boolean free;
+    private Machine NextMachine;
 
     // Constructor
+    public Machine(int machineId, int capacity, Machine Next) {
+        this.machineId = machineId;
+        this.capacity = capacity;
+        this.currentLoad = 0;
+        this.inventory = 0;
+        this.processTime = 5000; //(in milliseconds)
+        this.processVariance = 1000; //(in milliseconds)
+        this.free = true;
+        this.NextMachine = Next;
+    }
+    
     public Machine(int machineId, int capacity) {
         this.machineId = machineId;
         this.capacity = capacity;
         this.currentLoad = 0;
         this.inventory = 0;
-        this.processTime = 0;
-        this.processVariance = 0;
+        this.processTime = 5000; //(in milliseconds)
+        this.processVariance = 1000; //(in milliseconds)
         this.free = true;
+        //this.NextMachine = Next;
     }
 
     // Getter for machineId
@@ -53,38 +68,69 @@ public class Machine {
     } 
 
     // Start the machine
-    public void start() {
+    public void startMachine(int load) {
         System.out.println("Machine " + machineId + " started.");
+        processMaterials(load);
     }
 
     // Stop the machine
     public void stop() {
         System.out.println("Machine " + machineId + " stopped.");
+        //runIfInventoryAvailable();
     }
 
+    public int getRandomProcessVariance() {
+        Random random = new Random();
+        return (random.nextInt(2*processVariance) -processVariance);
+    }
     // Process materials
     public void processMaterials(int materials) {
-        if (currentLoad + materials <= capacity) {
-            currentLoad += materials;
-            System.out.println("Machine " + machineId + " is processing " + materials + " materials. Current load: " + currentLoad);
-        } else {
-            System.out.println("Machine " + machineId + " cannot process more materials. Capacity exceeded.");
+        free = false;
+        System.out.println("Machine " + machineId + " is processing " + materials);
+        int fullProcessTime = processTime + getRandomProcessVariance();
+        try {
+            Thread.sleep(fullProcessTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        free = true;
+        System.out.println("Full Process Time: " + fullProcessTime);
+        System.out.println("Processed materials totalling: " + materials);
+        stop();
+    }
+
+    public void loadMaterials(int materials, int machineId) {
+        int load;
+        if (free){
+            if (capacity < materials) {
+                load = capacity;
+            }
+            else {
+                load = materials;
+            }
+            inventory -= load;
+            startMachine(load);
+        }
+        else{
+            System.out.println("Machine is not current free");
         }
     }
 
     public void runIfInventoryAvailable() {
         if (inventory > 0 && free) {
-            start();
-            processMaterials(inventory);
-            stop();
-            inventory = inventory - capacity;
-            if (inventory < 0) {
-                inventory = 0;
-            }
+            //start();
+            loadMaterials(inventory, 1);
+            
             System.out.println("Inventory: " + inventory);
         } else {
             System.out.println("Machine " + machineId + " cannot run. Inventory is empty.");
         }
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        runIfInventoryAvailable();
     }
 
     public void runInLoop() {
@@ -103,11 +149,8 @@ public class Machine {
         // Start the thread
         thread.start();
     }
-    public static void main(String[] args) {
-        Machine machine1 = new Machine(1, 50);
-        machine1.addInventory(50);
-        machine1.addInventory(75);
-        machine1.runInLoop();
 
+    public static void main(String[] args) {
+        
     }
 }
---
+++ Machine Problem/MainCode.java
@@ -0,0 +1,12 @@
+public class MainCode{
+    public static void main(String[] args) {
+        Machine end = new Machine(4, 50);
+        Machine machine3 = new Machine(3, 50, end);
+        Machine machine2 = new Machine(2, 50, machine3);
+        Machine machine1 = new Machine(1, 50, machine2);
+        machine1.addInventory(275);
+        //machine1.addInventory(75);
+        machine1.runIfInventoryAvailable();
+    
+    }
+}
