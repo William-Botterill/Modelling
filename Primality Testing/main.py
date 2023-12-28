0218db951b1a1216d59afb041127fb48ee645457
William-Botterill <114944652+William-Botterill@users.noreply.github.com>
2023-12-28 11:17:06 +0000

    Only odd primality testing. Shows significant time improvement over simple primality testing as half as many possible divisors are tested.

--- Primality Testing/main.py
+++ Primality Testing/main.py
@@ -1,30 +1,72 @@
 import math
+import time
 
 
 def simple_primality_test(n):
     if n > 1:
-        max_test = int(math.sqrt(n))
+        max_test = int(math.sqrt(n)) + 1
         for i in range(2, max_test):
-            if n % 1 == 0:
-                return max_test
+            if n % i == 0:
+                return i
+        return 0
+    return 1
+
+
+def only_odd_primality_test(n):
+    if n > 1:
+        max_test = int(math.sqrt(n)) + 1
+        if n % 2 == 0:
+            return 2
+        for i in range(3, max_test, 2):
+            if n % i == 0:
+                return i
         return 0
     return 1
 
 
 def run_primality(n):
+
+    print("Simple primality testing")
+    start = time.time()
     output = simple_primality_test(n)
+    end = time.time()
+    elapsed = end - start
     if output == 0:
         print(n, " is a prime number!")
+        print(f"Calculated in: {elapsed:.6f} seconds. (Using simple primality testing)")
     elif output == 1:
         print(n, " is not a prime number. ")
+        print(f"Calculated in: {elapsed:.6f} seconds. (Using simple primality testing)")
     else:
-        print(n, " is divisible by ", str(output), ". Therefore it is not prime")
+        print(n, " is divisible by ", output, ". Therefore it is not prime.")
+        print(f"Calculated in: {elapsed:.6f} seconds. (Using simple primality testing)")
+
+    print("Only odd primality testing: ")
+    start = time.time()
+    output = only_odd_primality_test(n)
+    end = time.time()
+    elapsed = end - start
+    if output == 0:
+        print(n, " is a prime number!")
+        print(f"Calculated in: {elapsed:.6f} seconds. (Using only odd primality testing)")
+    elif output == 1:
+        print(n, " is not a prime number. ")
+        print(f"Calculated in: {elapsed:.6f} seconds. (Using only odd primality testing)")
+    else:
+        print(n, " is divisible by ", output, ". Therefore it is not prime.")
+        print(f"Calculated in: {elapsed:.6f} seconds. (Using only odd primality testing)")
 
 
 def main():
-    run_primality(5)
-    run_primality(2)
-    run_primality(1)
+    # run_primality(5)
+    # run_primality(2)
+    # run_primality(1)
+    # run_primality(4)
+    run_primality(7919)
+    run_primality(999983)
+    run_primality(9000011)
+    run_primality(800000000047)
+    run_primality(990000017087)
 
 
 if __name__ == '__main__':
